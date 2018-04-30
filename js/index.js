aec3f13311a22001940d5336f08904997a98bb8b
marovski <cardozo27cv@gmail.com>
2018-04-30 15:23:41 +0100

    firstCommit

---
+++ js/index.js
@@ -0,0 +1,137 @@
+//sOME ANIMATION TO INTROIMAGE 
+
+$(document).ready(function() {
+    
+    
+  $( "#links" ).hover(function() {
+  $( "#link" ).addClass("animated shake");
+});
+  
+  });
+
+
+$(document).on({
+    mouseenter: function () {
+       $( "#link" ).addClass("animated shake");
+    },
+    mouseleave: function () {
+       $( "#link" ).addClass("animated bounce");
+    }
+}, ".selector");
+
+
+//Contact Form Validator
+$(function() {
+
+  $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+    preventSubmit: true,
+    submitError: function($form, event, errors) {
+      // additional error messages or events
+    },
+    submitSuccess: function($form, event) {
+      event.preventDefault(); // prevent default submit behaviour
+      // get values from FORM
+
+      
+      
+      var message = $("textarea#message").val();
+      // var firstName = name; 
+      // For Success/Failure Message
+      // // Check for white space in name for Success/Fail message
+      // if (firstName.indexOf(' ') >= 0) {
+      //   firstName = name.split(' ').slice(0, -1).join(' ');
+      // }
+      $this = $("#sendMessageButton");
+      $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+    
+ 
+      
+      $.ajax({
+        url: "mailto:" + "cardozo27cv@gmail.com" + "?subject=" + message ,
+        type: "POST",
+        cache: false,
+        success: function() {
+          // Success message
+          $('#success').html("<div class='alert alert-success'>");
+          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+          $('#success > .alert-success')
+            .append("<strong>Your message has been sent. </strong>");
+          $('#success > .alert-success')
+            .append('</div>');
+          //clear all fields
+          $('#contactForm').trigger("reset");
+        },
+        error: function() {
+          // Fail message
+          $('#success').html("<div class='alert alert-danger'>");
+          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+          $('#success > .alert-danger').append($("<strong>").text("Sorry " + email + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append('</div>');
+          //clear all fields
+          $('#contactForm').trigger("reset");
+        },
+        complete: function() {
+          setTimeout(function() {
+            $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+          }, 1000);
+        }
+      });
+    },
+    filter: function() {
+      return $(this).is(":visible");
+    },
+  });
+
+  $("a[data-toggle=\"tab\"]").click(function(e) {
+    e.preventDefault();
+    $(this).tab("show");
+  });
+});
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+  $('#success').html('');
+});
+
+
+$(function() {
+	
+
+	/* Features */
+	$('.wp2').waypoint(function() {
+		$('.wp2').addClass('animated fadeIn');
+	}, {
+		offset: '50%'
+	});
+
+	/* Experience */
+	$('.wp3').waypoint(function() {
+		$('.wp3').addClass('animated fadeIn');
+		$('.skills-bar-container').addClass('animated fadeIn');
+        $('.wp3-1').addClass('progress1-delay');
+        $('.wp3-2').addClass('progress2-delay');
+        $('.wp3-3').addClass('progress3-delay');
+        $('.wp3-4').addClass('progress4-delay');
+        $('.wp3-5').addClass('progress5-delay');
+	}, {
+		offset: '50%'
+	});
+
+
+
+
+
+
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+
+
+});
